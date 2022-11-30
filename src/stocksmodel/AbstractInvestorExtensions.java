c46b577a0f077bbd949c8ef9546307fb5da7ccf7
viv-dan <81843793+viv-dan@users.noreply.github.com>
2022-11-30 16:34:05 -0500

    proper doc

--- src/stocksmodel/AbstractInvestorExtensions.java
+++ src/stocksmodel/AbstractInvestorExtensions.java
@@ -1,2 +1,44 @@
-package stocksmodel;public class AbstractInvestorExtensions {
+package stocksmodel;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+abstract class AbstractInvestorExtensions {
+
+  protected abstract String getFilename();
+
+  protected static Date getDateFromString(String date) {
+    Date intoDate;
+    try {
+      intoDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+    } catch (java.text.ParseException e) {
+      throw new RuntimeException("Invalid date " + e.getMessage());
+    }
+    return intoDate;
+  }
+
+  protected JSONObject readJSON() {
+    try (FileReader reader = new FileReader(this.getFilename())) {
+      JSONParser jsonParser = new JSONParser();
+      return (JSONObject) jsonParser.parse(reader);
+    } catch (IOException e) {
+      throw new RuntimeException("Cannot retrieve stored data " + e.getMessage());
+    } catch (ParseException e) {
+      throw new RuntimeException("parsing");
+    }
+  }
+
+  protected void writeToJSON(JSONObject data) {
+    try (FileWriter file = new FileWriter(this.getFilename())) {
+      file.write(data.toJSONString());
+    } catch (IOException e) {
+      throw new RuntimeException("Cannot store data!!");
+    }
+  }
 }
